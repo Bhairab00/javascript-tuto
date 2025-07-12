8e6544174f7cd9c3a9323084ce6c21aac9d5a27d
BHAIRAB BERA <112712305+Bhairab00@users.noreply.github.com>
2025-07-12 19:32:57 +0530

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,15 @@
+console.log("BHAIRAB")
+const nameid = 55666
+let str1 = "google"
+let number = 66
+
+accountID = 6655
+accountemail = "bhairab@google.com"
+accountpass = "123456"
+console.log(str1)
+console.table([ str1 , accountID,accountemail,accountpass]);
+
+let srt1 = "bhairab"
+let str2 = "  bera"
+let str3 = srt1 + str2
+console.log(str3)
